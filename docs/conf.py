f1f77987d5aa4c0296c3fec93b943c07c11f27d8
arcangelo7 <arcangelomas@gmail.com>
2021-11-05 19:32:32 +0100

    Version update in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Arcangelo Massari (https://orcid.org/0000-0002-8420-0696)'
 author = 'Arcangelo Massari (https://orcid.org/0000-0002-8420-0696)'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.1-beta'
+release = '2.2.3'
 
 
 # -- General configuration ---------------------------------------------------
