e456fa8fe1d92343ca60e830fcfa4def6f3c1d95
arcangelo7 <arcangelomas@gmail.com>
2022-04-03 15:49:12 +0200

    Version number

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Arcangelo Massari (https://orcid.org/0000-0002-8420-0696)'
 author = 'Arcangelo Massari (https://orcid.org/0000-0002-8420-0696)'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '3.1.0'
 
 
 # -- General configuration ---------------------------------------------------
